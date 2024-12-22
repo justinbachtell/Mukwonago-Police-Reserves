54f59b7746aa0a35e72dba2a619c45aefb7690f1
Justin Bachtell <justin@justinbachtell.com>
2024-12-22 12:01:46 -0600

    build(husky and semantic release): modified husky, commitlint, and semantic release
    Modified husky, commitlint, and added semantic release for better commit logs and versioning.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# The Landing App of the Village of Mukwonago Police Reserves
+# The Village of Mukwonago Police Department Reserves Program
+
+The Village of Mukwonago Police Reserves program is a community-based organization that provides law enforcement support services to the Village of Mukwonago.
 
 ### Live Site
 
--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,26 @@ import type { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 100],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert',
+        'ci',
+        'build',
+      ],
+    ],
+  },
 };
 
 export default Configuration;
